e22b6628777c9943a5d075126c73cf33536a6ec8
haingocpham19 <haingocpham@hotmail.com>
2022-04-27 18:26:05 +0700

    hom nay la thu 4

--- js/custom.js
+++ js/custom.js
@@ -24,26 +24,45 @@ function topFunction() {
   document.documentElement.scrollTop = 0;
 }
 //click show menu mobile
-document.getElementById("btn-more-ne").addEventListener("click", function(e) {
-    let check = document.querySelector('.more-menu').style.display;
-    if(check==="block"){
-      ShowHideMenuMobile();
-    }
-    else{
-      ShowMoreMenuMobile();
-    }
-})
-function ShowMoreMenuMobile(){
-  document.querySelector('.more-menu').setAttribute("style","display:block !important;");
-  document.querySelector('.overlay_menu').style.display="block";
-}
-function ShowHideMenuMobile(){
-  document.querySelector('.more-menu').setAttribute("style","display:none !important;");
-  document.querySelector('main').setAttribute("style","opacity:1");
-  document.querySelector('.overlay_menu').style.display="none";
-}
-//Hide OverLay
-document.querySelector(".overlay_menu").addEventListener("click", function(e) {
-  document.querySelector('.more-menu').setAttribute("style","display:none !important;");
-  document.querySelector('.overlay_menu').style.display="none";
-})
+// document.getElementById("btn-more-ne").addEventListener("click", function(e) {
+//     let check = document.querySelector('.more-menu').style.display;
+//     if(check==="block"){
+//       ShowHideMenuMobile();
+//     }
+//     else{
+//       ShowMoreMenuMobile();
+//     }
+// })
+// function ShowMoreMenuMobile(){
+//   document.querySelector('.more-menu').setAttribute("style","display:block !important;");
+//   document.querySelector('.overlay_menu').style.display="block";
+// }
+// function ShowHideMenuMobile(){
+//   document.querySelector('.more-menu').setAttribute("style","display:none !important;");
+//   document.querySelector('main').setAttribute("style","opacity:1");
+//   document.querySelector('.overlay_menu').style.display="none";
+// }
+// //Hide OverLay
+// document.querySelector(".overlay_menu").addEventListener("click", function(e) {
+//   document.querySelector('.more-menu').setAttribute("style","display:none !important;");
+//   document.querySelector('.overlay_menu').style.display="none";
+// })
+
+// function ChooseStepPayTut(){
+//   let queryC = document.querySelector('.home-pay-tutorial').querySelectorAll('.tut-step'); 
+//   let countStep = queryC.length;
+//   let arrayClassName = [];
+//   for (let i = 0; i < countStep; i++){
+//     arrayClassName.push(queryC[i].className);
+//   }
+//   let resultIndex = arrayClassName.indexOf('tut-step active');
+//   console.log(resultIndex);
+
+//   for(let i = 0; i < countStep; i++){
+//     document.querySelector('.img-step').querySelectorAll('img')[i].className = "tut-step";
+//   }
+// }
+// document.querySelector('.circle-step').addEventListener('click', {
+//   ChooseStepPayTut();
+// });
+
